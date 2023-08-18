cb6c351caa7b2d763dfd35f7a7d61aa760e5d4e1
Pedro <pedro.santos0908@eaportal.org>
2023-08-18 15:44:48 -0300

    mensagem para o commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- 
+ const [email, setEmail] = useState("")
+ const [senha, setSenha] = useState("")
+
+ function handleEmail(evento) {
+  setEmail(evento.target.value)
+ }
+
+function handleSenha(evento) {
+ setSenha(evento.target.value)
+}
+
+function handleSumit (){
+ alert(`
+  Email: ${email}
+  Senha: ${senha}
+ `)
+}
+
   return (
   <>
   <header>
@@ -10,16 +27,18 @@ function App() {
   </header>
 
   <main>
-  <form action="">
+  <form action="" onSubmit={handleSumit}>
     <div>
-      <label htmlFor="">Email</label>
-      <input type="email" id='email' placeholder='Digite seu email'/>
+      <label htmlFor="">Email:</label>
+      <input type="email" id='email' placeholder='Digite seu email' onChange={handleEmail}/>
       </div>
 
     <div>
       <label htmlFor="">Idade</label>
-      <input type="idade" id='idade' placeholder='Digite sua Idade' />
+      <input type="idade" id='idade' placeholder='Digite sua Idade' onChange={handleSenha} />
       </div>
+      
+      <button>Entrar</button>
 
   </form>
   </main>
